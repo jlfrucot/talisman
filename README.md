21d5dc268fcbe5ec20fbda88a088a97a0436c637
Jean-Louis Frucot <frucot.jeanlouis@free.fr>
2020-03-02 17:41:34 +0100

    Readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Talisman FabLab La-Bobine SDIS32
 
 Preuve de concept pour la gestion de rubans de leds depuis un navigateur web
+La page web permet de commander l'allumage et l'extinction d'un ruban de led géré par un arduino.
+L'ESP32 est configuré en serveur web
--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,7 @@
   @todo Changer de lib pour le serveur web ???
  */
 #include <Arduino.h>
-//#include <Adafruit_NeoPixel.h>
 #include <WiFi.h>
-//#include <WiFiClient.h>
-//#include <ArduinoOTA.h>
 #include <ESPmDNS.h>
 #include <WebServer.h>
 #include <Update.h>
@@ -33,9 +30,6 @@
 #define OUTPUT_SERIAL_PRINT 1 // 1 affiche les debugs, 0 ne les affiche pas
 
 #include "data.h"
-//#include "ruban.h"
-
-volatile bool interruptCounter;
 
 /////////////////////////////////////////// WIFI ///////////////////////
 // Déclaration d'un client pour le WIFI
@@ -60,19 +54,6 @@ bool yellowOn();
 bool yellowOff();
 #include "JLF_ota.h" // Pour déporter les "pages html" du server OTA
 
-/////////////////////////////////////////// Timer //////////////////////
-// #if OUTPUT_SERIAL_PRINT
-// unsigned long totalInterruptCounter;
-// #endif
-// hw_timer_t *timer = NULL;
-// portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-// // Fonction de callback pour le timer
-// void IRAM_ATTR onTimer()
-// {
-//   portENTER_CRITICAL_ISR(&timerMux);
-//   interruptCounter = true;
-//   portEXIT_CRITICAL_ISR(&timerMux);
-// }
 
 ////////////////////////////////////////////// LEDS NEOPIXELS ///////////
 const uint8_t LEDSPIN = 25;
@@ -91,18 +72,7 @@ void setup()
 {
 
   Serial.begin(9600);
-  // Déclaration et initialisation du timer
-  // timer = timerBegin(0, 80, true);
-  // timerAttachInterrupt(timer, &onTimer, true);
-  // timerAlarmWrite(timer, 2500000, true); // 25 ms
-  // timerAlarmEnable(timer);
-  // Initialisation du ruban
-  // m_ruban = new Ruban(LEDSCOUNT, LEDSPIN);
-  // m_ruban->setColor(m_color[0], m_color[1], m_color[2]);
-  // m_ruban->setMotif(m_motif);
-
-  // Déclaration d'un client pour le WIFI
-  // WiFiClient wifiClient;
+  
   setup_wifi();
   // Service mDNS pour la résolution de nom
   if (!MDNS.begin(HOST))
@@ -122,82 +92,13 @@ void setup()
   OTAServerInit(); // Initialise et lance le serveur web pour OTA (voir JLF_OTA.h)
   ////////// Tests
   pinMode(REDPIN, OUTPUT);
-  // digitalWrite(REDPIN, LOW);
-  // pinMode(GREENPIN, OUTPUT);
-  // pinMode(YELLOWPIN, OUTPUT);
+
 }
 
 void loop()
 {
   server.handleClient(); // Permet l'écoute du serveur http pour OTA
 
-  // if (client) {
-
-  //   if (client.connected()) {
-  //     Serial.println("Connected to client");
-  //   }
-
-  // // close the connection:
-  // client.stop();
-  // }
-//   if (interruptCounter)
-//   {
-//     // C'est ici que l'on demande au ruban d'avancer d'un pas
-//     portENTER_CRITICAL(&timerMux);
-//     interruptCounter = false;
-//     portEXIT_CRITICAL(&timerMux);
-
-//     ////////// TEst
-//     // Serial.println("digitalWrite");
-//     // digitalWrite(REDPIN, HIGH);
-// #if OUTPUT_SERIAL_PRINT
-//     totalInterruptCounter++; //
-//     Serial.print("An interrupt as occurred. Total number: ");
-//     Serial.println(totalInterruptCounter);
-// #endif
-//     //On gère le ruban m_stripe
-//         if (m_ruban->isStopped() == false) // on ne fait rien si le ruban est arrêté
-//         {
-//           if (m_nbBoucle == -1) // nombre de boucles infini
-//           {
-//             m_currentLed = m_ruban->nextStep();
-//     #if OUTPUT_SERIAL_PRINT
-//             Serial.println("m_ruban : boucle infinie");
-//     #endif
-//           }
-//           else
-//           {
-//             if (m_nbBoucle > 0) // Il reste des boucles à faire
-//             {
-//               m_currentLed = m_ruban->nextStep();
-//     #if OUTPUT_SERIAL_PRINT
-//               Serial.print("Boucle n°");
-//               Serial.print(m_nbBoucle);
-//               Serial.print("   Led n°");
-//               Serial.println(m_currentLed);
-//     #endif
-//               if (m_currentLed - LEDSCOUNT == 0) // A-t-on fini une boucle ?
-//               {
-//                 m_nbBoucle--; // boucle suivante
-//               }
-//             }
-//             else // C'est la dernière boucle
-//             {
-//               if (m_currentLed == LEDSCOUNT) //On est arrivé à la fin du ruban
-//               {
-//                 m_currentLed = m_ruban->stop();
-//     #if OUTPUT_SERIAL_PRINT
-//                 Serial.print("Plus de boucle");
-//                 Serial.print("     Arrêté à la Led n°");
-//                 Serial.println(m_currentLed);
-//                 Serial.print("m_stripe stoppé ");
-//                 Serial.println(m_ruban->isStopped());
-//     #endif
-//               }
-//             }
-//           }
-//         }
-//   }
 }
 
 void setup_wifi()
